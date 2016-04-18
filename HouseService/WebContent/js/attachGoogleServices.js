f9307b2c44a160057da57ae93fd5aa0189627f4a
soumyamishra89 <soumyamishra89@gmail.com>
2016-04-18 19:28:40 +0200

    added a js file to attach google services to html element

---
+++ HouseService/WebContent/js/attachGoogleServices.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+function attachGoogleSearch() {
+	var input = document.getElementById('googlesearch');
+	var googleSearchBox = new google.maps.places.SearchBox(input);
+	var searchButton = document.getElementById('searchButton');
+	searchButton.addEventListener("click", function() {
+		window.location.assign("search_result.html?searchplace="+input.value);
+	});
+}
+
+function enabledisablesearch() {
+	if(document.getElementById('googlesearch').value==="") {
+		searchButton.disabled = true;
+	} else {
+		searchButton.disabled = false;
+	}
+}
