06a46263edd57b000cc2c4996b3e4bf996128833
soumyamishra89 <soumyamishra89@gmail.com>
2016-04-18 14:28:15 +0200

    integrated the UI with the search code

--- HouseService/WebContent/js/googlesearch.js
+++ HouseService/WebContent/js/googlesearch.js
@@ -1,25 +1,42 @@
 /**
  * 
  */
-function addSearchBox() {
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.Autocomplete(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+var locationsearchBox, searchBox;
+// gets triggered when the value in locationSearch box is changed
+function enabledisableSearch() {
+	var searchButton = document.getElementById('googlesearchButton');
+	if(locationsearchBox.value === "" || placesToSearch.length===0)
+		searchButton.disabled = true;
+	else 
+		searchButton.disabled = false;
+}
 
+function addSearchBox() {
+	locationsearchBox = document.getElementById('locationSearch');
+	searchBox= new google.maps.places.SearchBox(locationsearchBox);
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(locationsearchBox);
+
+	// if a search place has been provided then it will be added to search box in this page
+	if(window.location.search!=="") {
+		console.log(window.location.search.split("=")[1]);
+		locationsearchBox.value=window.location.search.split("=")[1];
+		locationsearchBox.oninput();
+	}
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
+	map.addListener('bounds_changed', function() {
+		searchBox.setBounds(map.getBounds());
+	});
 
   //var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('place_changed', function() {
-    var place = searchBox.getPlace();
+  searchBox.addListener('places_changed', function() {
+	  var places = searchBox.getPlaces();
 
-    if (!place) {
-      return;
-    }
+	    if (places.length == 0) {
+	      return;
+	    }
+
     // Clear out the old markers.
    // markers.forEach(function(marker) {
     //  marker.setMap(null);
@@ -29,13 +46,14 @@ function addSearchBox() {
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
     
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+    places.forEach(function(place) {
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
 
       
       // Create a marker for each place.
@@ -52,7 +70,7 @@ function addSearchBox() {
       } else {
         bounds.extend(place.geometry.location);
       }
-   
+    });
       // after a place is searched the heatmaps are set
     map.setCenter(bounds.getCenter());
     heatmapData = new google.maps.MVCArray() ;
--- HouseService/WebContent/js/latlonginfo.js
+++ HouseService/WebContent/js/latlonginfo.js
@@ -1,69 +1,99 @@
 /**
  * 
  */
-var map, heatmap, loc, heatmapData;
-var placesToSearch= ['transport', 'grocery_or_supermarket', 'shopping_mall', 'restaurant'];
-var request = { radius: '500',
+var map, heatmap, loc, heatmapData, radius = 500;
+//var placesToSearch= ['publicTransport', 'grocery_or_supermarket', 'shopping_mall', 'restaurant'];
+var request = { radius: radius,
 		    rankby: 'prominence'
 		  };
+// function called on click of the search button	
+function searchMap(){
+	google.maps.event.trigger(locationsearchBox, 'focus')
+    google.maps.event.trigger(locationsearchBox, 'keydown', {
+        keyCode: 13
+    });
+}
 
+
+var placesToSearch = [];
+function addSearchCriteria() {
+	placesToSearch = [];
+	var placeOfInterests = document.getElementsByName('placeOfInterest');
+	for(var i=0, searchCriteria; searchCriteria=placeOfInterests[i]; i++) {
+		if(searchCriteria.checked) {
+			placesToSearch.push(searchCriteria.value);
+		}
+	}
+	var searchButton = document.getElementById('googlesearchButton');
+    if(placesToSearch.length === 0) {
+    	searchButton.disabled = true;
+    } else {
+    	searchButton.disabled = false;
+    }
+}
 /**
  * Loads the heatmap as per the points of interest specified by the user
  */
 function loadHeatMap() {
-    
     request['location'] = map.getCenter();
-	var xhttp = new XMLHttpRequest();
-	var nearbyStopsURL = [];
-	if(placesToSearch.indexOf('transport')!=-1) {
-		nearbyStopsURL.push("https://api.resrobot.se/location.nearbystops?key=0eab2bb0-a7dc-420e-9385-eda6641ad913");
-		nearbyStopsURL.push("&format=json&maxNo=1000");
+    
+	for(var i = 0, criteria; criteria = placesToSearch[i]; i++) {
+		switch(criteria) {
+			case 'publicTransport' :
+					var xhttp = new XMLHttpRequest();
+					var nearbyStopsURL = [];
+					nearbyStopsURL.push("https://api.resrobot.se/location.nearbystops?key=0eab2bb0-a7dc-420e-9385-eda6641ad913");
+					nearbyStopsURL.push("&format=json&maxNo=1000");
+					
+					nearbyStopsURL.push("&originCoordLat="+ map.getCenter().lat()+"&originCoordLong="+map.getCenter().lng());
+					nearbyStopsURL.push("&r="+radius)
+					//  xhttp.open("GET", "http://api.sl.se/api2/nearbystops/Json?key=509ecd92161d475fa8db07d3eed30e26&originCoordLat=59.293611&originCoordLong=18.083056&maxresults=1000&radius=2000", true);
+					xhttp.open("GET", nearbyStopsURL.join(""), true)  
+					xhttp.send();
+					  
+					xhttp.onreadystatechange = function() {
+					  if (xhttp.readyState == 4 && xhttp.status == 200) {
+					   	var response = xhttp.responseText;
+					   	if(response.indexOf("error")==-1) {
+					   		var stopsInfo = JSON.parse(response);
+					   		var weight = placesToSearch.length-placesToSearch.indexOf('publicTransport');
+					   		
+					   		for(var i=0, stopLoc;stopLoc=stopsInfo.StopLocation[i];i++){
+					   			heatmapData.push({location: new google.maps.LatLng(stopLoc.lat, stopLoc.lon), weight:weight});	
+					   		}
+					    }
+					    	//document.getElementById("demo").innerHTML = heatmapData;
+					  }
+			
+					}; 
+					break;
 		
-		nearbyStopsURL.push("&originCoordLat="+ map.getCenter().lat()+"&originCoordLong="+map.getCenter().lng());
-		nearbyStopsURL.push("&r=500")
-		//  xhttp.open("GET", "http://api.sl.se/api2/nearbystops/Json?key=509ecd92161d475fa8db07d3eed30e26&originCoordLat=59.293611&originCoordLong=18.083056&maxresults=1000&radius=2000", true);
-		xhttp.open("GET", nearbyStopsURL.join(""), true)  
-		xhttp.send();
-		  
-		xhttp.onreadystatechange = function() {
-		  if (xhttp.readyState == 4 && xhttp.status == 200) {
-		   	var response = xhttp.responseText;
-		   	if(response.indexOf("error")==-1) {
-		   		var stopsInfo = JSON.parse(response);
-		   		var weight = placesToSearch.length-placesToSearch.indexOf('transport');
-		   		
-		   		for(var i=0, stopLoc;stopLoc=stopsInfo.StopLocation[i];i++){
-		   			heatmapData.push({location: new google.maps.LatLng(stopLoc.lat, stopLoc.lon), weight:weight});	
-		   		}
-		    }
-		    	//document.getElementById("demo").innerHTML = heatmapData;
-		  }
-		}; 
-	}
-	
-	if(placesToSearch.indexOf('grocery_or_supermarket')!=-1) {
-		request['type']='grocery_or_supermarket'
-			
-	  	service = new google.maps.places.PlacesService(map);
-	  	service.radarSearch(request, function (results, status) {
-	  		addPlaceToHeatMap(results, status, 'grocery_or_supermarket');
-	  	});
-	}
-	if(placesToSearch.indexOf('shopping_mall')!=-1) {
-		request['type']='shopping_mall'
-			
-	  	service = new google.maps.places.PlacesService(map);
-	  	service.radarSearch(request, function (results, status) {
-	  		addPlaceToHeatMap(results, status, 'shopping_mall');
-	  	});
-	}
-	if(placesToSearch.indexOf('restaurant')!=-1) {
-		request['type']='restaurant'
-	  	
-	  	service = new google.maps.places.PlacesService(map);
-	  	service.radarSearch(request, function (results, status) {
-	  		addPlaceToHeatMap(results, status, 'restaurant');
-	  	});
+			case 'grocery_or_supermarket' :
+					request['type']='grocery_or_supermarket'
+						
+				  	service = new google.maps.places.PlacesService(map);
+				  	service.radarSearch(request, function (results, status) {
+				  		addPlaceToHeatMap(results, status, 'grocery_or_supermarket');
+				  	});
+				  	break;
+			case 'shopping_mall' :
+					request['type']='shopping_mall'
+						
+				  	service = new google.maps.places.PlacesService(map);
+				  	service.radarSearch(request, function (results, status) {
+				  		addPlaceToHeatMap(results, status, 'shopping_mall');
+				  	});	
+				  	break;
+		
+			case 'restaurant' :
+					request['type']='restaurant'
+				  	
+				  	service = new google.maps.places.PlacesService(map);
+				  	service.radarSearch(request, function (results, status) {
+				  		addPlaceToHeatMap(results, status, 'restaurant');
+				  	});
+				  	break;
+		}
 	}
 }
 	/*var map = new google.maps.Map(document.getElementById('map'), {
@@ -72,6 +102,7 @@ function loadHeatMap() {
 	    scrollwheel: false
 	});*/
 
+// callback called when the map is loaded
 function initMap() {
 	loc = new google.maps.LatLng(59.333344, 18.056963999999994);
 	// adding location of search for request to place search
@@ -82,12 +113,12 @@ function initMap() {
     mapTypeId: google.maps.MapTypeId.MAP
   });
 	// once a map has been created, a searchbox to search for places is attached to it. check googlesearch.js
-	addSearchBox();
+	
 	// initialise a heatmap with only map. The data would be loaded once a place has been searched
     heatmap = new google.maps.visualization.HeatmapLayer({
         map: map
     });
-    
+    addSearchBox();
 }
 	
     /*function getPoints() {
