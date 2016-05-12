b2d58a4881f29f8d04388572b1a294ab1439d083
Sudarson <ssudarson@gmail.com>
2016-05-13 01:11:24 +0200

    WaterMark added

--- HouseService/WebContent/js/imageconverter.js
+++ HouseService/WebContent/js/imageconverter.js
@@ -2,13 +2,22 @@ $(function() {
     $("#btnSave").click(function() { 
         html2canvas($("#detailedLocationInfo"), {
             onrendered: function(canvas) {
-                var a = document.createElement('a');
+                
                 // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
-                a.href = canvas.toDataURL("image/png");//canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+                //a.href = canvas.toDataURL("image/png");//canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+                
+                $img = canvas.toDataURL("image/png");
+                $('#contentImg').append("<img src="+$img+" class='watermark_img' id='insitemap_img_id'/>");
+                $('#insitemap_img_id').watermark({
+                	  text: 'insitemap.net',
+                	  textWidth: 100
+                	});
+         
+                var a = document.createElement('a');
+                a.href = $('#insitemap_img_id').attr('src');
                 a.download = 'insitemap.jpg';
                 a.click();
-            },
-            background:"#C8846E"
+            }
         });
     });
 }); 
