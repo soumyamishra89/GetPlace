8db729741b7c6f70b063dd78a38040ea7fb79fcd
soumyamishra89 <soumyamishra89@gmail.com>
2016-05-13 15:28:24 +0200

    watermark added to the download image

--- HouseService/WebContent/js/imageconverter.js
+++ HouseService/WebContent/js/imageconverter.js
@@ -6,21 +6,31 @@ $(function() {
                 // toDataURL defaults to png, so we need to request a jpeg, then convert for file download.
                 //a.href = canvas.toDataURL("image/png");//canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
                 
-                $img = canvas.toDataURL("image/png");
+                $img = watermarkedDataURL(canvas, "insitemap.net");//canvas.toDataURL("image/png");
                 $('#contentImg').attr("src", $img);
-                $('#contentImg').append("<img src="+$img+" class='watermark_text' id='insitemap_img_id'/>");
-                $('#insitemap_img_id').watermark({
-                //$('#contentImg').watermark({
-                	  text: 'insitemap.net',
-                	  textWidth: 100,
-                	  textColor: 'white'
-                	});
-         
                 var a = document.createElement('a');
-                a.href = $('#insitemap_img_id').attr('src');
+                a.href = $('#contentImg').attr('src');
                 a.download = 'insitemap.png';
                 a.click();
             }
         });
     });
+    
+    function watermarkedDataURL(canvas,text){
+    	  var tempCanvas=document.createElement('canvas');
+    	  var tempCtx=tempCanvas.getContext('2d');
+    	  var cw,ch;
+    	  cw=tempCanvas.width=canvas.width;
+    	  ch=tempCanvas.height=canvas.height;
+    	  tempCtx.drawImage(canvas,0,0);
+    	  tempCtx.font="20px verdana";
+    	  var textWidth=tempCtx.measureText(text).width;
+    	  tempCtx.globalAlpha=.30;
+    	  tempCtx.fillStyle='black'
+    	  tempCtx.fillText(text,cw-textWidth,ch-12);
+    	  // just testing by adding tempCanvas to document
+    	  //document.body.appendChild(tempCanvas);
+    	  return(tempCanvas.toDataURL("image/png"));
+    	}
+
 }); 
